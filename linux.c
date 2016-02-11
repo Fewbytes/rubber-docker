4e6653c647b8a05fc0234fdde4908276da733f30
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2016-02-11 18:39:39 +0200

    pivot_root works

--- linux.c
+++ linux.c
@@ -7,29 +7,26 @@ pivot_root(PyObject *self, PyObject *args) {
 	const char *put_old, *new_root;
 	int ret;
 
-	if (!PyArg_ParseTuple(args, "s", &new_root))
-        return NULL;
-    if (!PyArg_ParseTuple(args, "s", &put_old))
-    	return NULL;
+	if (!PyArg_ParseTuple(args, "ss", &new_root, &put_old))
+		return NULL;
 
-    ret = syscall(SYS_pivot_root, &new_root, &pivot_root);
+	ret = syscall(SYS_pivot_root, new_root, put_old);
 
-    if (ret == -1) {
-    	PyErr_SetFromErrno(PyEx_RuntimeError)
-    	return NULL;
-    }
-    
-    return NULL;
+	if (ret == -1) {
+		PyErr_SetFromErrno(PyExc_RuntimeError);
+		return NULL;
+	}
+	Py_INCREF(Py_None);
+	return Py_None;
 }
 
 static PyMethodDef LinuxMethods[] = {
-    {"pivot_root",  pivot_root, METH_VARARGS,
-     "pivot_root system call"},
+	{"pivot_root", pivot_root, METH_VARARGS, "pivot_root system call"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 PyMODINIT_FUNC
 initlinux(void)
 {
-    (void) Py_InitModule("linux", LinuxMethods);
+	(void) Py_InitModule("linux", LinuxMethods);
 }
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 module1 = Extension('linux', sources=['linux.c'])
 
