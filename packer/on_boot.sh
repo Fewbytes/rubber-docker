9cb1c8a33f34dd8a8fc0b1a7729368aae7967aab
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2016-02-25 12:00:07 +0200

    add swap to machines on boot

--- packer/on_boot.sh
+++ packer/on_boot.sh
@@ -3,3 +3,9 @@
 mkdir -p /workshop/containers
 
 chown ubuntu:ubuntu -R /workshop
+
+if [[ $(cat /proc/swaps | wc -l) -le 1 && ! -f /swap.img ]]; then
+	dd if=/dev/zero of=/swap.img count=1024 bs=1M
+	mkswap /swap.img
+	swapon /swap.img
+fi
