cfba3090a073acc93fffbe40f9eadfd2b2c547ed
Mick Pollard <aussielunix@gmail.com>
2016-06-18 15:36:34 +1000

    fix /swap.img permissions during vagrant bootstrap

--- packer/on_boot.sh
+++ packer/on_boot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 mkdir -p /workshop/containers
 
@@ -6,6 +6,7 @@ chown ubuntu:ubuntu -R /workshop
 
 if [[ $(cat /proc/swaps | wc -l) -le 1 && ! -f /swap.img ]]; then
 	dd if=/dev/zero of=/swap.img count=1024 bs=1M
+  chmod 0600 /swap.img
 	mkswap /swap.img
 	swapon /swap.img
 fi
