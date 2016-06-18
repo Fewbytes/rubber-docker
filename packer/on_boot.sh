d68c03f73291e154381c330d0eadfc832d32f0c3
Mick Pollard <aussielunix@gmail.com>
2016-06-18 20:10:07 +1000

    removed debug from on_boot.sh and cleaned code indentation

--- packer/on_boot.sh
+++ packer/on_boot.sh
@@ -1,12 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
 mkdir -p /workshop/containers
 
 chown ubuntu:ubuntu -R /workshop
 
 if [[ $(cat /proc/swaps | wc -l) -le 1 && ! -f /swap.img ]]; then
-	dd if=/dev/zero of=/swap.img count=1024 bs=1M
+  dd if=/dev/zero of=/swap.img count=1024 bs=1M
   chmod 0600 /swap.img
-	mkswap /swap.img
-	swapon /swap.img
+  mkswap /swap.img
+  swapon /swap.img
 fi
+
