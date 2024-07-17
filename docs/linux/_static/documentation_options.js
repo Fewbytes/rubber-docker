51c350b63b81a2e512baeba29aa3750e38646e55
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2024-07-17 14:54:34 +0300

    new docs build

--- docs/linux/_static/documentation_options.js
+++ docs/linux/_static/documentation_options.js
@@ -1,9 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+const DOCUMENTATION_OPTIONS = {
     VERSION: '0.1',
-    LANGUAGE: 'None',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
