5fcdc341b446ad5a46a3f41f50f3fcf50c21044b
teidorS <lilpunz3l@gmail.com>
2025-01-01 16:06:59 +0530

    chore: add fork and exec commands to code

--- levels/00_fork_exec/rd.py
+++ levels/00_fork_exec/rd.py
@@ -30,6 +30,7 @@ def contain(command):
     #       https://docs.python.org/2/library/os.html#os.execv
     # NOTE: command is an array (the first element is path/file, and the entire
     #       array is exec's args)
+    os.execv(command[0], command)
 
     os._exit(0)  # TODO: remove this after adding exec
 
@@ -39,7 +40,7 @@ def contain(command):
 def run(command):
     # TODO: replace this with fork()
     #       (https://docs.python.org/2/library/os.html#os.fork)
-    pid = 0
+    pid = os.fork()
     if pid == 0:
         # This is the child, we'll try to do some containment here
         try:
